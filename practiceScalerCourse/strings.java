cdac5fe23fbf72ee052cdcb9ca70a8b810bf62a4
TrinayBhati <bhatiji780780@gmail.com>
2022-12-29 20:56:08 +0530

    29th dec 2022

--- practiceScalerCourse/strings.java
+++ practiceScalerCourse/strings.java
@@ -12,13 +12,46 @@ public class strings {
             System.out.print(ch);
         }
     }
+
+    public static void reverseVowels(String str){
+        char[] ch = str.toCharArray();
+
+        int i = 0; 
+        int j = ch.length - 1;
+        while(i<j){
+            while(ch[i] != 'a' && ch[i] != 'e' && ch[i] != 'i' && ch[i] != '0' && ch[i] != 'u'){
+                i++;
+            }
+            while(ch[j] != 'a' && ch[j] != 'e' && ch[j] != 'i' && ch[j] != '0' && ch[j] != 'u'){
+                j--;
+            }
+            if(i>=j){
+                break;
+            }
+            char c = ch[i];
+            ch[i] = ch[j];
+            ch[j] = c;
+            i++;
+            j--;
+            
+        }
+        for(char c:ch){ //this is how we take every element from array
+                System.out.print(c);
+            };
+
+    }
     public static void main(String[] args) {
         
         //remove vowels from a given string
         String str = "youcandoit";
         removeVowels(str);
-
+        System.out.println();
         //reverse Vowels in given String
+        String str1 = "youcandoit";
+        reverseVowels(str1);
+        //convert upper case to lower case
+        
+
         
     }
     
