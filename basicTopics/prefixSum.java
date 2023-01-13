cd93964d2ae1180a47efbaa023aa18ce7da88c7e
TrinayBhati <bhatiji780780@gmail.com>
2023-01-13 21:05:50 +0530

    13jan

---
+++ basicTopics/prefixSum.java
@@ -0,0 +1,8 @@
+public static int[] prefixSum(int nums[]){
+    int presum[] = new int[nums.length];
+    presum[0] = nums[0];
+    for(int i = 1; i<nums.length ; i++){
+        presum[i] = presum[i-1] + nums[i];
+    }
+    return presum;
+}
