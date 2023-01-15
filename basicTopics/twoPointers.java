83bb8ac916cdff5ba4970d3085e73e3a103adde4
TrinayBhati <bhatiji780780@gmail.com>
2023-01-15 23:17:22 +0530

    15th-jan

--- basicTopics/twoPointers.java
+++ basicTopics/twoPointers.java
@@ -45,3 +45,42 @@ public class twoPointers {
 
     }
 }
+
+//===================================================================================
+
+// The two pointers approach is a technique used to solve problems that involve arrays. The basic idea behind this approach is to use two pointers to traverse the array, one pointer starting at the beginning of the array and the other pointer starting at the end of the array. The two pointers move towards each other, depending on the conditions of the problem, until they meet in the middle or a specific condition is met.
+
+// Here's an example of using the two pointers approach to solve a problem:
+
+
+// Given an array of integers, find the pair of indices whose elements have the smallest difference.
+
+
+// int[] arr = {4, 2, 1, 8, 3, 9};
+// int minDiff = Integer.MAX_VALUE;
+// int left = 0, right = arr.length - 1;
+// int leftIndex = 0, rightIndex = 0;
+
+// Arrays.sort(arr);
+// while (left < right) {
+//     int diff = arr[right] - arr[left];
+//     if (diff < minDiff) {
+//         minDiff = diff;
+//         leftIndex = left;
+//         rightIndex = right;
+//     }
+//     if (arr[left] < arr[right]) {
+//         left++;
+//     } else {
+//         right--;
+//     }
+// }
+
+// System.out.println("The pair of indices whose elements have the smallest difference are: " + leftIndex + " and " + rightIndex);
+// In the above example, we first sort the array to make it easy to compare the values. Then, we use two pointers, left and right, to traverse the array. We initialize left pointer to the first element of the array and right pointer to the last element of the array. In each iteration, we calculate the difference between the values at the left and right pointers. If the difference is less than the current minimum difference, we update the minimum difference and the indices. Then, we check which value is smaller, the one at the left pointer or the one at the right pointer. If the value at the left pointer is smaller, we increment the left pointer, otherwise, we decrement the right pointer. We continue this process until the left pointer is greater than or equal to the right pointer.
+
+// Two pointers approach is very efficient in solving problems that involve arrays, especially when the array is sorted.It can be used to solve problems like two sum, three sum, closest pair of elements, finding duplicate elements and many more.It can also be used to solve problems in which you have to find the first or last occurrence of an element in an array.
+
+// It's important to note that the solution of the problem may be different depending on the direction of traversing the array, if you start from left pointer or right pointer.
+
+// In general, the two pointers approach can be a good choice when you need to traverse an array in a specific order, and when you can use the order of the elements in the array to your advantage.
--- sorting/mergeSort.java
+++ sorting/mergeSort.java
@@ -1,58 +1,85 @@
-//package sorting;
-//
-//public class mergeSort {
-//
-//    public static void merge(int arr[] , int f, int mid , int e){
-//        int merged[] = new int[e - f + 1];
-//
-//        int a1 = f;
-//        int a2 = mid + 1;
-//        int a = 0;
-//        while(a1 <= mid && a2 <= e){
-//            if(arr[a1] <= arr[a2]){
-//                merged[a] = arr[a1];
-//                a++ ; a1++;
-//            }
-//            else{
-//                merged[a] = arr[a2];
-//                a++ ; a2++;
-//            }
-//        }
-//        while(a1 <= mid){
-//            merged[a] = arr[a1];
-//            a++ ; a1++;
-//        }
-//        while(a2 <= e){
-//            merged[a] = arr[a2];
-//            a++ ; a2++;
-//        }
-//        for(int i = 0 , j = f ; i<merged.length ; i++ , j++){
-//                arr[j] = merged[i];
-//        }
-//    }
-//    public static void divide(int arr[] ,  int f , int e){
-//        if(f<e){
-//            int mid = f + (e-f) / 2;
-//            divide(arr , f , mid  );
-//            divide(arr , mid + 1 , e );
-//            merge(arr, f , mid , e);
-//        }
-//    }
-//    public static void main(String[] args) {
-//      int arr[] = { 12, 11, 13, 5, 6, 7};
-//      int n = arr.length;
-//
-//      divide(arr ,0 , n-1 );
-//
-//      for(int i = 0 ; i <n ; i++){
+package sorting;
+
+public class mergeSort {
+
+   public static void merge(int arr[] , int f, int mid , int e){
+       int merged[] = new int[e - f + 1];
+
+       int a1 = f;
+       int a2 = mid + 1;
+       int a = 0;
+       while(a1 <= mid && a2 <= e){
+           if(arr[a1] <= arr[a2]){
+               merged[a] = arr[a1];
+               a++ ; a1++;
+           }
+           else{
+               merged[a] = arr[a2];
+               a++ ; a2++;
+           }
+       }
+       while(a1 <= mid){
+           merged[a] = arr[a1];
+           a++ ; a1++;
+       }
+       while(a2 <= e){
+           merged[a] = arr[a2];
+           a++ ; a2++;
+       }
+       for(int i = 0 , j = f ; i<merged.length ; i++ , j++){
+               arr[j] = merged[i];
+       }
+   }
+   public static void divide(int arr[] ,  int f , int e){
+       if(f<e){
+           int mid = f + (e-f) / 2;
+           divide(arr , f , mid  );
+           divide(arr , mid + 1 , e );
+           merge(arr, f , mid , e);
+       }
+   }
+   public static void main(String[] args) {
+     int arr[] = { 12, 11, 13, 5, 6, 7};
+     int n = arr.length;
+
+     divide(arr ,0 , n-1 );
+
+     for(int i = 0 ; i <n ; i++){
+         System.out.println(arr[i] + " ");
+     }
+       System.out.println();
+   }
+
+}
+
+//=============================================================================
+
+// The main method starts by creating an array of integers arr: int arr[] = { 12, 11, 13, 5, 6, 7}; and initializing a variable n with the length of the array.
+
+// The divide method is called with the input array, arr, and the starting and ending indices of the array, 0 and n-1 respectively: divide(arr ,0 , n-1 );
+
+// Inside the divide method, the first thing it does is check the base case: if the starting index is greater than the ending index, then the portion of the array is already sorted and the method returns.
+
+// If the starting index is less than the ending index, it calculates the middle index mid by adding the starting and ending indices and dividing by 2: int mid = f + (e-f) / 2;
+
+// Next, the divide method recursively calls itself for the left and right half of the array:
+// divide(arr , f , mid );
+// divide(arr , mid + 1 , e );
+
+// Once both halves of the array have been divided and sorted, the merge method is called to merge the two sorted halves back together: merge(arr, f , mid , e);
+
+// Inside the merge method, it creates a new temporary array merged with the same number of elements as the portion of the array to be merged. It then initializes two pointers, a1 and a2, to the f and mid + 1 indices of the portion of the array to be merged, respectively. And it initializes a which will be used as an index for the merged array.
+
+// Then it enters a loop where it compares the elements at the a1 and a2 indices and adds the smaller of the two elements to the merged array at the a index.
+
+// After the merge process is done, the elements in the merged array are copied back to the input array.
+
+// The recursive call of divide method will continue until the base case is reached and each time when merge method is called it will merge two sorted halves of the array.
+
+// Once the divide method has returned, the main method prints the sorted array to the console by iterating through the array and printing each element:
+
+// for(int i = 0 ; i <n ; i++){
 //          System.out.println(arr[i] + " ");
 //      }
-//        System.out.println();
-//
-//
-//
-//
-//
-//    }
-//
-//}
+     
+// And then the program ends.
