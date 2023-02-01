94843762419a3034d7c3087f25398f7feca95365
TrinayBhati <bhatiji780780@gmail.com>
2023-02-01 09:03:36 +0530

    1-feb

---
+++ IMPORTANTS/PracticeAlgo.java
@@ -0,0 +1,77 @@
+
+
+class binarySearch{
+	public static boolean isSorted(int arr[]){
+		for(int i = 0; i< arr.length ; i++){
+			if(arr[i] > arr[i+1]){
+				return false;
+			}
+		}
+			 return true;
+	}
+	public static int binary(int arr[] , int x){
+		
+		int f = 0;
+		int l = arr.length-1;
+		while( f<=l){
+			int mid = f + (l-f)/2;
+			if(x == arr[mid]){
+				return mid;
+			}
+			else if(x>arr[mid]){
+				f = mid +1;
+			}
+			else{
+				l = mid-1;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		int arr[] = {1, 3, 5, 6, 8, 12, 19, 22, 45, 987, 5765};
+		int x = 12;
+		System.out.println(binarySearch(arr, x));
+	}
+}
+
+//====================================================================
+
+class linearSearch{
+	public static int search(int arr[] , int x){
+		for(int i = 0 ; i< arr.length ; i++){
+			if(arr[i] == x)
+				return i;
+			}
+				return -1;
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {21,43,56,66,678,32};
+		int x = 56;
+		System.out.println(search(arr, x));
+	}
+}
+
+//========================================================
+
+class binarySearch{
+	public static void main(String[] args) {
+		int arr[] = {2, 3, 5, 6, 8, 12, 90, 990};
+		int x = 8;
+		int n = arr.length;
+		int mid;
+		int f = 0 ; int l = n-1;
+
+		for(int i = 0 ; i< n ; i++){
+			int mid = f+(l-f)/2;
+			if(arr[mid] == x){return mid;
+				}			
+			else if(arr[mid] < x){f =  mid +1;
+				}					
+			if{
+				l = mid-1;
+				}
+		}
+		return -1;
+	}
+}
---
+++ linkedListStackQueues/practiceLL.java
@@ -0,0 +1,12 @@
+/*
+collection of linked nodes
+first node of ll is called head
+data then address of next node
+temp node temp 
+temp = head
+temp != null
+array fast access time O(1)
+ll not fast takes o(n)
+insertion and deletion is faster in ll as we dont have to shift all element from any side
+
+*/
