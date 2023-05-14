5f0e1d3c532f901fe872b5a3402a3202fb3325e4
Trinay Bhati <bhatiji780780@gmail.com>
2023-05-14 22:28:02 +0530

    14-may

--- sorting/bubbleSort.java
+++ sorting/bubbleSort.java
@@ -8,7 +8,7 @@ public class bubbleSort {
         int n = a.length;
         
         for(int i = 0; i<n-1 ; i++){
-            for(int j=0; j<n-i-1 ; j++ ){
+            for(int j =0; j<n-i-1 ; j++ ){
                 if(a[j] > a[j+1]){
                     //swap
                     int temp = a[j];
