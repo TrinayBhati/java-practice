48e91aea1716c897313905fbc9689501f46492d5
Trinay Bhati <bhatiji780780@gmail.com>
2023-05-16 17:58:53 +0530

    16-may

--- sorting/insertionSort.java
+++ sorting/insertionSort.java
@@ -59,3 +59,18 @@ public class insertionSort {
         }
     }
 }
+
+
+class Sort{
+    public static void sorting(int arr[]) {
+        for(int i = 1; i<arr.length ; i++){
+            int current = arr[i];
+            int j = i-1;
+            while(j>=0 && current <arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }    
+    }
+}
